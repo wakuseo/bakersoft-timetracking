e1fa3ef19d465fddd20ce03eb9dcc1a7b91af37b
Serhat Teker <serhat.teker@gmail.com>
2021-11-01 22:13:44 +0300

    Update populate_db script

--- populate_db.py
+++ populate_db.py
@@ -15,10 +15,14 @@ USER = get_user_model()
 
 # account=admin, id 1
 admin = USER.objects.get(id=1)
+
 project = Project.objects.create(account=admin, manager=admin, name="Bake")
 work = Work.objects.create(account=admin, project=project, name="mold")
 time.sleep(2)
-work.completed = True
-work.status = "done"
-work.save()
+work.complete()
 work = Work.objects.create(account=admin, project=project, name="ferment")
+
+project_2 = Project.objects.create(account=admin, manager=admin, name="Sale")
+work_2 = Work.objects.create(account=admin, project=project_2, name="advertise")
+time.sleep(2)
+work_2.complete()
