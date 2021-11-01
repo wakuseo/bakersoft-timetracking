3f0de6c201d15a7c0df0cc16b34f0fdb8849d5e8
Serhat Teker <serhat.teker@gmail.com>
2021-11-01 18:28:05 +0300

    Add tracking admin module

---
+++ bakersoft/trackings/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from bakersoft.trackings.models import Project, Work
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["name", "status", "manager", "completed", "elapsed_time"]
+    list_filter = ["created_at", "changed_at"]
+    search_fields = [
+        "name",
+        "account__email",
+        "account__username",
+    ]
+
+
+@admin.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ["name", "status", "completed", "completed_at", "elapsed_time"]
+    list_filter = ["created_at", "changed_at"]
+    search_fields = [
+        "name",
+        "account__email",
+        "account__username",
+    ]
