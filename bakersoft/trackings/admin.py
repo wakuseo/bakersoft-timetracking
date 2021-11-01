62e0413e5c0a0b7bab2f380b2fc4d04486111b72
Serhat Teker <serhat.teker@gmail.com>
2021-11-01 22:13:22 +0300

    Add trackings admin

--- bakersoft/trackings/admin.py
+++ bakersoft/trackings/admin.py
@@ -5,7 +5,7 @@ from bakersoft.trackings.models import Project, Work
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ["name", "status", "manager", "completed", "elapsed_time"]
+    list_display = ["name", "status", "manager", "completed", "elapsed_time", "uuid"]
     list_filter = ["created_at", "changed_at"]
     search_fields = [
         "name",
@@ -16,7 +16,7 @@ class ProjectAdmin(admin.ModelAdmin):
 
 @admin.register(Work)
 class WorkAdmin(admin.ModelAdmin):
-    list_display = ["name", "status", "completed", "completed_at", "elapsed_time"]
+    list_display = ["name", "status", "completed", "completed_at", "elapsed_time", "uuid"]
     list_filter = ["created_at", "changed_at"]
     search_fields = [
         "name",
